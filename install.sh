219ead92728973e3f4041e076c760de2ac0b163f
Encoder Backup <backup@encoder.com>
2025-11-25 08:56:07 -0300

    Backup final de configuracao. Excluido arquivo 2402.tgz via .gitignore.

---
+++ install.sh
@@ -0,0 +1,18 @@
+   #!/bin/bash
+   apt update
+   apt install git curl build-essential wget -y
+   rm -f /etc/systemd/system/docker.service.d/override.conf
+   update-grub
+   curl -fsSL https://get.docker.com -o get-docker.sh
+   sh get-docker.sh
+   VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+   DESTINATION=/usr/local/bin/docker-compose
+   sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+   ls -l /usr/local/bin/docker-compose && chmod 755 /usr/local/bin/docker-compose
+   /usr/local/bin/docker-compose -v
+   ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+   mkdir /opt/ssl && mkdir /opt/conf && chmod 755 /opt/ssl && chmod 755 /opt/conf
+   
+systemctl daemon-reload \
+  && sudo systemctl restart docker
+  systemctl restart docker
